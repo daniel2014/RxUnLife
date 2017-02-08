557d9855768676d6a9edcdb3a124efe5ad46707c
SatoShun <shun.sato1@gmail.com>
2017-02-08 11:45:02 +0000

    fix UnlifeTransformer comment

--- rxunlife/src/main/java/com/github/satoshun/rx/unlife/UnLifeTransformer.java
+++ rxunlife/src/main/java/com/github/satoshun/rx/unlife/UnLifeTransformer.java
@@ -3,22 +3,18 @@ package com.github.satoshun.rx.unlife;
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 
-import rx.Completable;
 import rx.Observable;
 import rx.Single;
 
 /**
- * A Transformer that works for all RxJava types ({@link Observable}, {@link Single} and {@link Completable}).
+ * A Transformer that works for two RxJava types ({@link Observable}, {@link Single}).
  * <p>
- * Out of the box, it works for Observable. But it can be easily converted
- * for {@link Single} or for {@link Completable}.
+ * Out of the box, it works for Observable. But it can be easily converted for {@link Single}.
  */
 public interface UnLifeTransformer<T> extends Observable.Transformer<T, T> {
 
   /**
    * @return a version of this Transformer for {@link Single} streams.
-   * <p>
-   * If interrupted by the lifecycle, this stream throws onError({@link java.util.concurrent.CancellationException}).
    */
   @Nonnull
   @CheckReturnValue
